460c22ca15f24f70dc75d1839fb7f2cd224b051c
JhonKTorres <12-11135@usb.ve>
2017-01-26 14:52:00 -0400

    Archivo faltante para la version 2

---
+++ Tarea2/objeto.py
@@ -0,0 +1,11 @@
+import datetime
+
+class tiempoDeServicio:
+    def __init__(self, t1, t2):
+        self.tiempoInicioDeServicio = t1
+        self.tiempoFinalDeServicio = t2
+        
+class tarifa:
+    def __init__(self, t1, t2):
+        self.tarifaSemana = t1
+        self.tarifaFinSemana = t2
